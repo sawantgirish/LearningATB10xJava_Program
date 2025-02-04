c40d70d9ed75140c4880186b1c68fb4bf016e649
sawantgirish <gbsawant7378@gmail.com>
2025-02-04 20:47:06 +0530

    11th jan_2025

--- src/ex_01_JavaBascis/Lab011_Common_Math_Exception.java
+++ src/ex_01_JavaBascis/Lab011_Common_Math_Exception.java
@@ -4,7 +4,7 @@ public class Lab011_Common_Math_Exception
 {
     public static void main(String[] args) {
         System.out.println();
-        System.out.println(10/0);//java.lang.ArithmeticException
+        System.out.println(10 / 0);//java.lang.ArithmeticException
 //        System.out.println("Exception is a Abnormal Behaviour in a program which Basically Happens Due to some Problem");
         System.out.println("here we cannot devide 10/0 so the JVM will give a Exception error");
     }
---
+++ src/ex_02_JavaBasics/Lab024_Println_VS_print.java
@@ -0,0 +1,19 @@
+package ex_02_JavaBasics;
+
+public class Lab024_Println_VS_print
+{
+    public static void main(String[] args) {
+        int a=12;
+        System.out.println();
+        System.out.printf("variable name is %d",a);
+
+        /* %d ->  it is used for -. int,byte,long,short
+        *  %s -> It is used for STRING
+        *  %f -> it is used for FLOat And Boolean
+        *  if we mention %d,%f,%s in any printing statement using 'printf' then the variable value will be replaced according to the DATATYPE
+        *
+         */
+
+
+    }
+}
