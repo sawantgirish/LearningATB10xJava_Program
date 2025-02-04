213ff6d02316e13f313e1664e10db003d36d5489
sawantgirish <gbsawant7378@gmail.com>
2025-02-04 20:34:06 +0530

    11th jan_2025

---
+++ src/ex_02_JavaBasics/Lab019_Interview_QnA.java
@@ -0,0 +1,12 @@
+package ex_02_JavaBasics;
+
+public class Lab019_Interview_QnA
+{
+    public static void main(String[] args) {
+        int _q =11;
+        int  _2 = 23;
+        int abc_cuiefh$fh =23;
+        // this is a valid code ;
+        System.out.println("This is a Valid code");
+    }
+}
---
+++ src/ex_02_JavaBasics/Lab020_Interview_QnA2.java
@@ -0,0 +1,13 @@
+package ex_02_JavaBasics;
+
+public class Lab020_Interview_QnA2
+{
+    public static void main(String[] args) {
+        ///int enum = 20;
+
+
+       // System.out.println(enum); //enum is a keyword so this code is not valid
+        System.out.println("enum is a keyword so this code is not valid");
+
+    }
+}
