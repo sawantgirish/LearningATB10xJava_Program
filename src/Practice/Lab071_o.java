a3bb098979f6c7448f9ba6880c668833d5db9a2b
Girish <gbsawant7378@gmail.com>
2025-02-22 09:57:57 +0530

    19th jan_2025

---
+++ src/Practice/Lab071_o.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class Lab071_o
+{
+    public static void main(String[] args) {
+
+       webdriver
+    System.out.println("");
+    }
+}
--- src/ex_06_Ternary_Operator/Lab070.java
+++ src/ex_06_Ternary_Operator/Lab070.java
@@ -1,4 +1,7 @@
 package ex_06_Ternary_Operator;
 
 public class Lab070 {
+    public static void main(String[] args) {
+
+     }
 }
