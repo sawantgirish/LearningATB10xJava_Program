0133b51737dc9bcbaf3a7f5ff6a5b8233598b92d
Girish <gbsawant7378@gmail.com>
2025-04-04 12:12:05 +0530

    19th jan_2025

---
+++ src/ex_07/Lab072_ID.java
@@ -0,0 +1,21 @@
+package ex_07;
+
+public class Lab072_ID
+{
+    public static void main(String[] args) {
+        int a = 10;
+        int result = ++a;
+        System.out.println(a);
+        System.out.println(result);
+
+        // Method
+        // Line No. | Exp(result)  |  a
+        //  5 |  NA  | 10
+        // 6  | 11 | 11
+
+
+
+
+
+    }
+}
